21ab0327b52e26b2d34080dcde17e92f146dda40
Sergei Johansen <sergeitheprogrammer@gmail.com>
2022-08-28 20:13:31 +0200

    Initial setup

---
+++ data/data.js
@@ -0,0 +1,44 @@
+const data = {
+
+    home: {
+        title: "Utvikler med lidenskap for det logiske og det visuelle",
+        about: "eg har studert og drevet med google og facebook annonsering siden 2017. Jeg har jobbet med rørlegger-, og snekkerfirmaer, restauranter og skjønnhetssalonger. Nå studerer jeg programmering ved NTNU og jobber med web-teknologier og digital annonsering ved siden av studiene."
+    },
+
+    projects: {
+        text: "Todos os seres humanos nascem livres e iguais em dignidade e direitos. São dotados de razão e consciência e devem agir em relação uns aos outros com espírito de fraternidade.",
+
+        projectList: [{
+            title: "Nano Detailing",
+            techStack: "Wordpress & Elementor",
+            siteURL: "https://nanodetailing.no/"
+        }, {
+            title: "Oriental Thaimassasje",
+            techStack: "ReactJS & NextJS",
+            siteURL: "https://www.orientalthaimassasje.no/"
+        }]
+    },
+
+    services: {
+        list: [{
+            title: "Nettsted",
+            text: "Todos os seres humanos nascem livres e iguais em dignidade e direitos. São dotados de razão e consciência e devem agir em relação uns aos outros com espírito de fraternidade. Todos os "
+        }, {
+            title: "Google Ads",
+            text: "Todos os seres humanos nascem livres e iguais em dignidade e direitos. São dotados de razão e consciência e devem agir em relação uns aos outros com espírito de fraternidade. Todos os "
+        }]
+    },
+
+    contact: {
+        title: "Contact",
+        text: "Todos os seres humanos nascem livres e iguais em dignidade e direitos. São dotados de razão e consciência e devem agir em relação uns aos outros com espírito de fraternidade. Todos os ",
+        email: "medialics@gmail.com",
+        phone: "96703326",
+        address: "Sorgenfriveien 30a, 7031 Trondheim"
+    },
+
+    socialURLs: {
+        github: "https://github.com/sergeiThe",
+        linkedin: "https://www.linkedin.com/in/sergei-johansen-b731b0189/"
+    }
+}
